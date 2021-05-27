fd6922e61914f6714da433f1e22c190ac26637f7
ganhaobo <ganhaobo@meicai.cn>
2021-05-28 01:10:31 +0800

    chore: IndexedDB大部分完成，缺乏实际场景处理内容

--- BOM,DOM/BOM/存储/IndexedDB/easy-idb.js
+++ BOM,DOM/BOM/存储/IndexedDB/easy-idb.js
@@ -22,7 +22,6 @@ class EasyIDB {
 class EasyStore {
     constructor(store) {
         this.store = store
-        this.queue = []
     }
 
     add(value, key) {
@@ -56,7 +55,79 @@ class EasyStore {
     }
 
     createIndex(...args) {
-        return this.store.createIndex(...args)
+        return new EasyIndex(this.store.createIndex(...args))
+    }
+
+    index(name) {
+        return new EasyIndex(this.store.index(name))
+    }
+
+    // 游标的使用方式不同，每次迭代的结果都在同一个事件订阅中处理，
+    // 所以此时我们通过传入一个函数控制
+    useCursor({ query, direction, next } = {}) {
+        const request = this.store.openCursor(query, direction)
+
+        request.onsuccess = ({ target: { result } }) => result && next(result)
+
+        request.onerror = e => next(e)
+    }
+}
+
+class EasyIndex {
+    #MODE_TO_METHOD = {
+        value: 'openCursor',
+        key: 'openKeyCursor'
+    }
+
+    constructor(index) {
+        this._index = index
+    }
+
+    count() {
+        return wrapPromise((s, j) => {
+            const r = this._index.count()
+
+            r.onsuccess = ({ target: { result } }) => s(result)
+            r.onerror = e => j(e)
+        })
+    }
+
+    get(key) {
+        return wrapPromise((s, j) => {
+            const r = this._index.get(key)
+
+            r.onsuccess = ({ target: { result } }) => s(result)
+            r.onerror = e => j(e)
+        })
+    }
+
+    getAll(...args) {
+        return wrapPromise((s, j) => {
+            const r = this._index.getAll(...args)
+
+            r.onsuccess = ({ target: { result } }) => s(result)
+            r.onerror = e => j(e)
+        })
+    }
+
+    getAllKeys(...args) {
+        return wrapPromise((s, j) => {
+            const r = this._index.getAllKeys(...args)
+
+            r.onsuccess = ({ target: { result } }) => s(result)
+            r.onerror = e => j(e)
+        })
+    }
+
+    useCursor({ query, direction, next, mode = 'value' } = {}) {
+        const request = this._index[this.#MODE_TO_METHOD[mode]](
+            query,
+            direction
+        )
+
+        request.onsuccess = ({ target: { result } }) => result && next(result)
+
+        request.onerror = e => next(e)
     }
 }
 
@@ -66,13 +137,22 @@ function wrapPromise(cb) {
     return p
 }
 
-function open(name, version, { upgrade } = {}) {
+function open(name, version, { upgrade, versionchange, blocking } = {}) {
     return wrapPromise((resolve, reject) => {
         const request = indexedDB.open(name, version)
 
-        request.onsuccess = ({ target: { result } }) =>
+        request.onsuccess = ({ target: { result } }) => {
+            if (versionchange) {
+                result.onversionchange = e => versionchange(e)
+            }
             resolve(new EasyIDB(result), request)
+        }
+
         request.onerror = e => reject(e)
+
+        if (blocking) {
+            request.onblocked = e => blocking(e)
+        }
 
         // 处理数据库升级
         if (upgrade) {
